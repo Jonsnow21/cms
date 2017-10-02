eb1746a7dc5441c1b04c3dbefd510b366bb99255
relentless-coder <contact@ayushbahuguna.com>
2017-10-02 23:29:23 +0530

    read all the sensitive information from the environment variables

--- server/config/package-config.js
+++ server/config/package-config.js
@@ -1,7 +1,7 @@
 export const config = {
-	host: 'email-smtp.us-west-2.amazonaws.com',
-	port: 465,
-	mailUser: 'AKIAJ6YVBWF65JCGNWCA',
-	mailPass: 'Ag7hPnZdJism4zxra8K4/886te8O+EplzTqXWX1N/33C',
-	secret: 'inav'
+	host: process.env.mailHost,//'email-smtp.us-west-2.amazonaws.com',
+	port: process.env.mailPort,//465,
+	mailUser: process.env.mailUser,//'AKIAJ6YVBWF65JCGNWCA',
+	mailPass:  process.env.mailPass, //Ag7hPnZdJism4zxra8K4/886te8O+EplzTqXWX1N/33C,
+	secret: process.env.SECRET //inav
 }
